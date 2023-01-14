331eb94247a49da975402ab6cc29d9d93be789be
Ignacio <ignaciogarciareyna@live.com>
2023-01-14 02:21:30 -0300

    Se actualizó studies

--- src/components/Studies.js
+++ src/components/Studies.js
@@ -10,31 +10,35 @@ const Studies = () => {
       <div className="section-container">
         <div className="section-content">
           <div className="section-header">
-            <h2>Hola, soy Nacho!</h2>
-            <p>Acerca de mi:</p>
+            <h2>Programación</h2>
+            <p>Cómo llegué al mundo de la programación?</p>
           </div>
           <div className="section-text">
             <p>
-              En junio de 2019, luego de haber hecho un curso de Reparación de
-              PC y otro de Redes y Telecomunicaciones, decidí comenzar a
-              estudiar programación.
+              En de 2019, luego de haber hecho un curso de Reparación de PC y
+              otro de Redes y Telecomunicaciones, decidí comenzar a estudiar
+              programación y me anoté en la Licenciatura en Informática.
             </p>
           </div>
           <div className="section-text">
             <p>
-            En 2021 Realicé el Programa Up de Codellege by Softtek. En este
-              curso no solo aprendí desarrollo web con Angular, también tuve
-              clases de Inglés gramatical y conversacional y me certifiqué como Scrum
-              Developer.
-                <br/>
+              En 2021 cursé el Programa Up de Codellege by Softtek. En este
+              curso me enseñaron Angular, tuve clases de Inglés gramatical y
+              conversacional y me certifiqué como Scrum Developer.
+            </p>
+          </div>
+          <div className="section-text">
+            <p>
               En 2022 realicé un curso de React de Udemy dictado por Juan Pablo
-              de la Torre Valdez. Donde aprendí el uso de hooks y realicé mas de
-              10 proyectos. El haber aprendido Angular primero hizo que todo
-              fuera mas facil.
-                <br/>
-              Desde junio de 2022 trabajo como QA testeando componentes de un
-              CMS de Adobe. Trabajamos con un sistema de tickets de Jira. Además
-              de estar aprendiendo mucho, estoy cursando Inglés in Company con Lingua University.
+              de la Torre Valdez donde aprendí el uso de hooks y realicé mas de
+              10 proyectos.
+            </p>
+          </div>
+          <div className="section-text">
+            <p>
+              Desde junio de 2022 trabajo como QA en MRM. Testeo componentes de
+              Quantum (un CMS de Adobe). Además de estar aprendiendo mucho,
+              estoy cursando Inglés in Company con Lingua University.
             </p>
           </div>
           <div>
