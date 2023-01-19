cc555a318b0c02fb1108fb09b3eefcefb1c5653b
Ignacio <ignaciogarciareyna@live.com>
2023-01-18 21:04:19 -0300

    Cambio de estilo en resume

--- src/components/ResumeItem.js
+++ src/components/ResumeItem.js
@@ -7,7 +7,7 @@ const ResumeItem = ({ item }) => {
       <h4 className="item-title">{item.title}</h4>
       <p className="item-subtitle">{item.subtitle}</p>
       {item.text !== ""
-        ? item.lines.map((line) => <p className="item-text">{line}</p>)
+        ? item.lines.map((line) => <p className="item-text">· {line}</p>)
         : null}
     </div>
   );
