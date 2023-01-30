d6777acbfee2da9a71f874e6968ee36231b1e1ef
Ignacio <ignaciogarciareyna@live.com>
2023-01-30 16:29:59 -0300

    Cambio de social icons desde footer a contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,10 @@
 import React from "react";
 import emailjs from "emailjs-com";
 import { Link } from "react-scroll";
+import linkedinIcon from "../assets/img/linkedin-icon-white.png";
+import githubIcon from "../assets/img/github-icon-white.png";
+import hackerRank from "../assets/img/fake-hackerrank.png";
+import mailIcon from "../assets/img/mail-icon.png";
 
 const Contact = () => {
   const enviarMail = (e) => {
@@ -26,6 +30,34 @@ const Contact = () => {
           {/* <p>Something</p>  */}
           <div className="separator-line"></div>
         </header>
+        <ul className="container-icons">
+          <li className="social-link">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/ignaciogarciareyna/"
+            >
+              <img src={linkedinIcon} className="social-icon" />
+            </a>
+          </li>
+          <li className="social-link">
+            <a target="_blank" href="https://github.com/IgnacioGarciaReyna">
+              <img src={githubIcon} className="social-icon" />
+            </a>
+          </li>
+          <li className="social-link">
+            <a
+              target="_blank"
+              href="https://www.hackerrank.com/ignaciogarciare1"
+            >
+              <img src={hackerRank} className="social-icon" />
+            </a>
+          </li>
+          <li className="social-link">
+            <a href="mailto:ignaciogarciareyna@live.com">
+              <img src={mailIcon} className="social-icon" />
+            </a>
+          </li>
+        </ul>
         <div className="form-container">
           <form className="contact-form" onSubmit={enviarMail}>
             <input
@@ -54,7 +86,11 @@ const Contact = () => {
               rows="8"
             ></textarea>
 
-            <button id="submit-contact-button" type="submit" className="unstyle">
+            <button
+              id="submit-contact-button"
+              type="submit"
+              className="unstyle"
+            >
               Enviar
             </button>
           </form>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,14 @@
 import React from "react";
-import linkedinIcon from "../assets/img/linkedin-icon-white.png";
-import githubIcon from "../assets/img/github-icon-white.png";
-import hackerRank from "../assets/img/fake-hackerrank.png";
-import mailIcon from "../assets/img/mail-icon.png";
+
 
 const Footer = () => {
   return (
     <footer id="footer">
-      <ul className="container-icons">
-        <li className="social-link">
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/ignaciogarciareyna/"
-          >
-            <img src={linkedinIcon} className="social-icon" />
-          </a>
-        </li>
-        <li className="social-link">
-          <a target="_blank" href="https://github.com/IgnacioGarciaReyna">
-            <img src={githubIcon} className="social-icon" />
-          </a>
-        </li>
-        <li className="social-link">
-          <a target="_blank" href="https://www.hackerrank.com/ignaciogarciare1">
-            <img src={hackerRank} className="social-icon" />
-          </a>
-        </li>
-        <li className="social-link">
-          <a href="mailto:ignaciogarciareyna@live.com">
-            <img src={mailIcon} className="social-icon" />
-          </a>
-        </li>
-      </ul>
+      <div>
+        <p>2023 Ignacio Manuel Garcia Reyna</p>
+        <p>Desarrollado con React</p>
+        <p>Diseñado por Ignacio Manuel Garcia Reyna</p>
+      </div>
     </footer>
   );
 };
