dffcc2d22acf80b8ae33231fcd7945fcc6c9ea2d
Ignacio <ignaciogarciareyna@live.com>
2023-01-30 17:14:57 -0300

    Estilo del Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-
 const Footer = () => {
   return (
     <footer id="footer">
-      <div>
-        <p>2023 Ignacio Manuel Garcia Reyna</p>
-        <p>Desarrollado con React</p>
-        <p>Diseñado por Ignacio Manuel Garcia Reyna</p>
+      <div className="footer-content-container">
+        <p>
+          2023 © Diseñado y Desarrollado por Ignacio Manuel Garcia Reyna con
+          React
+        </p>
       </div>
     </footer>
   );
