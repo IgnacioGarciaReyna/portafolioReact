c9b540af7933046892c88a19e955148432e381ea
Ignacio <ignaciogarciareyna@live.com>
2023-02-13 18:27:27 -0300

    ResumeItems transition was added

--- src/components/ResumeItem.js
+++ src/components/ResumeItem.js
@@ -1,18 +1,73 @@
 import React from "react";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 const ResumeItem = ({ item }) => {
   return (
     <div className="resume-item">
-      <p className="item-duration">{item.duration}</p>
-      <h4 className="item-title">{item.title}</h4>
-      <p className="item-subtitle">{item.subtitle}</p>
+      <SwitchTransition>
+        <CSSTransition
+          classNames="fade"
+          key={item.duration}
+          addEndListener={(node, done) =>
+            node.addEventListener("transitionend", done, false)
+          }
+        >
+          <p className="item-duration">{item.duration}</p>
+        </CSSTransition>
+      </SwitchTransition>
+      <SwitchTransition>
+        <CSSTransition
+          classNames="fade"
+          key={item.title}
+          addEndListener={(node, done) =>
+            node.addEventListener("transitionend", done, false)
+          }
+        >
+          <h4 className="item-title">{item.title}</h4>
+        </CSSTransition>
+      </SwitchTransition>
+
+      <SwitchTransition>
+        <CSSTransition
+          classNames="fade"
+          key={item.subtitle}
+          addEndListener={(node, done) =>
+            node.addEventListener("transitionend", done, false)
+          }
+        >
+          <p className="item-subtitle">{item.subtitle}</p>
+        </CSSTransition>
+      </SwitchTransition>
+
       {item.lines !== ""
-        ? item.lines.map((line) => <p className="item-text">· {line}</p>)
+        ? item.lines.map((line) => (
+            <SwitchTransition>
+              <CSSTransition
+                classNames="fade"
+                key={item.lines}
+                addEndListener={(node, done) =>
+                  node.addEventListener("transitionend", done, false)
+                }
+              >
+                <p className="item-text">· {line}</p>
+              </CSSTransition>
+            </SwitchTransition>
+          ))
         : null}
       {item.reference.text ? (
-        <a className="item-text" target="_blank" href={item.reference.link}>
-          {item.reference.text}
-        </a>
+        <SwitchTransition>
+          <CSSTransition
+            classNames="fade"
+            key={item.reference.text}
+            addEndListener={(node, done) =>
+              node.addEventListener("transitionend", done, false)
+            }
+          >
+            <a className="item-text" target="_blank" href={item.reference.link}>
+              {item.reference.text}
+            </a>
+          </CSSTransition>
+        </SwitchTransition>
       ) : null}
     </div>
   );
