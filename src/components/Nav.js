1e06b2ca4cfe5a678573cf594bb133316702ae0d
Ignacio <ignaciogarciareyna@live.com>
2023-02-13 16:17:10 -0300

    Transition was added to about

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,7 @@ import { AiOutlineTranslation } from "react-icons/ai";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import LanguageContext from "../context/LanguageContext";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 AOS.init();
 
@@ -34,93 +35,163 @@ const Navigation = () => {
       <nav id="nav" data-aos="fade-down" data-aos-duration="2000">
         <ul className="nav-items-container" ref={navRef}>
           <li>
-            <Link
-              to="home"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-              className="nav-item"
-              onClick={hideNavbar}
-            >
-              {texts.nav.home}
-            </Link>
+            <SwitchTransition>
+              <CSSTransition
+                classNames="fade"
+                key={texts.nav.home}
+                addEndListener={(node, done) =>
+                  node.addEventListener("transitionend", done, false)
+                }
+              >
+                <Link
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                  className="nav-item"
+                  onClick={hideNavbar}
+                >
+                  {texts.nav.home}
+                </Link>
+              </CSSTransition>
+            </SwitchTransition>
           </li>
           <li>
-            <Link
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-              className="nav-item"
-              onClick={hideNavbar}
-            >
-              {texts.nav.about}
-            </Link>
+            <SwitchTransition>
+              <CSSTransition
+                classNames="fade"
+                key={texts.nav.about}
+                addEndListener={(node, done) =>
+                  node.addEventListener("transitionend", done, false)
+                }
+              >
+                <Link
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                  className="nav-item"
+                  onClick={hideNavbar}
+                >
+                  {texts.nav.about}
+                </Link>
+              </CSSTransition>
+            </SwitchTransition>
           </li>
           <li>
-            <Link
-              to="projects"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-              className="nav-item"
-              onClick={hideNavbar}
-            >
-              {texts.nav.projects}
-            </Link>
+            <SwitchTransition>
+              <CSSTransition
+                classNames="fade"
+                key={texts.nav.projects}
+                addEndListener={(node, done) =>
+                  node.addEventListener("transitionend", done, false)
+                }
+              >
+                <Link
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                  className="nav-item"
+                  onClick={hideNavbar}
+                >
+                  {texts.nav.projects}
+                </Link>
+              </CSSTransition>
+            </SwitchTransition>
           </li>
           <li>
-            <Link
-              to="resume"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-              className="nav-item"
-              onClick={hideNavbar}
-            >
-              {texts.nav.resume}
-            </Link>
+            <SwitchTransition>
+              <CSSTransition
+                classNames="fade"
+                key={texts.nav.resume}
+                addEndListener={(node, done) =>
+                  node.addEventListener("transitionend", done, false)
+                }
+              >
+                <Link
+                  to="resume"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                  className="nav-item"
+                  onClick={hideNavbar}
+                >
+                  {texts.nav.resume}
+                </Link>
+              </CSSTransition>
+            </SwitchTransition>
           </li>
           <li>
-            <Link
-              to="technologies"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-              className="nav-item"
-              onClick={hideNavbar}
-            >
-              {texts.nav.technologies}
-            </Link>
+            <SwitchTransition>
+              <CSSTransition
+                classNames="fade"
+                key={texts.nav.technologies}
+                addEndListener={(node, done) =>
+                  node.addEventListener("transitionend", done, false)
+                }
+              >
+                <Link
+                  to="technologies"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                  className="nav-item"
+                  onClick={hideNavbar}
+                >
+                  {texts.nav.technologies}
+                </Link>
+              </CSSTransition>
+            </SwitchTransition>
           </li>
           <li>
-            <Link
-              to="contact"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-              className="nav-item"
-              onClick={hideNavbar}
-            >
-              {texts.nav.contact}
-            </Link>
+            <SwitchTransition>
+              <CSSTransition
+                classNames="fade"
+                key={texts.nav.contact}
+                addEndListener={(node, done) =>
+                  node.addEventListener("transitionend", done, false)
+                }
+              >
+                <Link
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                  className="nav-item"
+                  onClick={hideNavbar}
+                >
+                  {texts.nav.contact}
+                </Link>
+              </CSSTransition>
+            </SwitchTransition>
           </li>
-          <li className="nav-item li-translate">
-            <button
-              className="translate-button"
-              onClick={toggleLanguage}
-              value={languageValue}
+          <SwitchTransition>
+            <CSSTransition
+              classNames="fade"
+              key={texts.nav.translate}
+              addEndListener={(node, done) =>
+                node.addEventListener("transitionend", done, false)
+              }
             >
-              {texts.nav.translate}
-              <AiOutlineTranslation className="icon-translate"></AiOutlineTranslation>
-            </button>
-          </li>
+              <li className="nav-item li-translate">
+                <button
+                  className="translate-button"
+                  onClick={toggleLanguage}
+                  value={languageValue}
+                >
+                  {texts.nav.translate}
+                  <AiOutlineTranslation className="icon-translate"></AiOutlineTranslation>
+                </button>
+              </li>
+            </CSSTransition>
+          </SwitchTransition>
           <FaTimes className="nav-close-menu" onClick={showNavMenu} />
         </ul>
         <FaBars className="nav-button-menu" onClick={showNavMenu} />
