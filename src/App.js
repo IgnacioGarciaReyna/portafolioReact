a3c0e34b81c2f3db83f0e335d290c2293e60bfd2
Ignacio <ignaciogarciareyna@live.com>
2023-02-24 16:16:21 -0300

    Some components were deleted

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import Jobs from "./components/Jobs";
 import Navigation from "./components/Nav";
-import Programming from "./components/Programming";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Technologies from "./components/Technologies";
-import University from "./components/University";
 import { LanguageProvider } from "./context/LanguageContext";
 
 function App() {
@@ -19,9 +16,6 @@ function App() {
         <Home />
         <About />
         <Projects />
-        {/* <University />
-      <Programming />
-      <Jobs /> */}
         <Resume />
         <Technologies />
         <Contact />
